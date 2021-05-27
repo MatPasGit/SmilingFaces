5d49158ce53935efd60f32bb1a16b10fc68ace9b
pelswick100 <pelswick100@wp.pl>
2021-05-27 20:47:48 +0200

    Almost everything to zad1

--- zad1.py
+++ zad1.py
@@ -1,35 +1,209 @@
+#Importy
 import numpy as np
 from RandomNumberGenerator import RandomNumberGenerator
 import math
+import random
+
+#Zmienne
+
+seed = 20  # ziarno
+n = 10  # liczba zadań
+maxIter = 100  # maksymalna liczba iteracji
+
+
+#DWA PIERWSZE KRYTERIA: 1 (makespan) i 3 (max tardiness)
+# Makespan - czas zakończenia wszystkich zadań
+# Max tardiness - maksymalne spóźnienie zadania
+
+def acceptProbability(i, it):
+    if i == 0:
+        return 0.1
+    else:
+        return 0.995 ** it
+
+def randomSolution():
+    #obliczanie losowego rozwiązania
+    x = []
+    for j in range(n):
+        x.append(j)
+
+    random.shuffle(x)
+    return x
+
+def checkSolution(x, tablep, tabled):
+    #funkcji obliczających wartości obu kryteriów (tzn. wartość funkcji celu jest parą liczb)
+    C = generateHarmonogram(x, tablep)
+
+    #Makespan
+    makespan = max(C)
+
+    #MaxTardiness
+    scoresMaxTardiness = []
+    count = 0
+    for j in x:
+        scoresMaxTardiness.append(max(C[count]-tabled[j],0))
+        count += 1
+    maxTardiness = max(scoresMaxTardiness)
+
+    return makespan, maxTardiness
+
+def generateHarmonogram(x, tablep):
+    #funkcji obliczającej harmonogram(wartości Cij)
+    result = []
+
+    C = []
+    C = [0 for i in range(3)]
+
+    firstTask = True
+    for j in x:
+        for i in range(3):
+
+            if i == 0 and firstTask:
+                C[i] = tablep[i][j]
+            elif i == 0 and not(firstTask):
+                C[i] = C[i] + tablep[i][j]
+            elif i > 0 and firstTask:
+                C[i] = C[i-1] + tablep[i][j]
+            elif i > 0 and not(firstTask):
+                C[i] = max(C[i-1], C[i]) + tablep[i][j]
+
+        result.append(C[2])
+        firstTask = False
+    return result
+
+def movefunction(type, x):
+    #funkcji ruchu (do generacji sąsiadów, najprościej wykorzystać sąsiedztwa insert lub swap),
+    array = x.copy()
+
+    if type == "insert":
+        a = random.randint(0, n-1)
+        b = 0
+        while a == b:
+            b = random.randint(0, n-1)
+
+        buf = array[a]
+        array[a] = array[b]
+        array[b] = buf
+
+    elif type == "swap":
+        a = random.randint(0, n-1)
+        b = 0
+        while math.fabs(a-b) <= 1:
+            b = random.randint(0, n-1)
+
+        if (a < b):
+            c = []
+            for i in range(a, b):
+                c.append(x[i])
+            c.reverse()
+            for i in range(a, b):
+                x[i] = c[i-a]
+        else:
+            c = []
+            for i in range(b, a):
+                c.append(x[i])
+            c.reverse()
+            for i in range(b, a):
+                x[i] = c[i-a]
+
+    return array
+
+def domination(makespan, maxTardiness, makespanPrim, maxTardinessPrim):
+    #obliczanie funkcji dominacji
+    if (makespanPrim <= makespan) and (maxTardinessPrim <= maxTardiness):
+        if (makespanPrim < makespan) or (maxTardinessPrim < maxTardiness):
+            return True
+    return False
+
+def frontParetoF(P, tablep, tabled):
+
+    F = []
+    Fscore = []
+    for a in P:
+        makespan, maxTardiness = checkSolution(a, tablep, tabled)
+        zmienna = True
+        for b in P:
+            if not(np.array_equiv(a, b)):
+                makespanPrim, maxTardinessPrim = checkSolution(b, tablep, tabled)
+                if domination(makespan, maxTardiness, makespanPrim, maxTardinessPrim):
+                    zmienna = False
+            if zmienna == False:
+                break
+        if zmienna:
+            F.append(a)
+            Fscore.append([makespan, maxTardiness])
+    return F, Fscore
+
+def calculateHVI():
+    return 0
 
 def main():
-    seed = 20           #ziarno
-    n = 4               #liczba zadań
 
-    #Zmienne przechowujące wylosowane dane
-    tablep = []
-    tablep = [[0 for j in range(n)] for i in range(3)]
-    tabled = []
-    tabled = [0 for j in range(n)]
+    maxIters = [100, 200, 400, 800, 1600]
 
+    for iteration in range(10):
+        for maxIter in maxIters:
+            #Zmienne przechowujące wylosowane dane
+            tablep = []
+            tablep = [[0 for j in range(n)] for i in range(3)]
+            tabled = []
+            tabled = [0 for j in range(n)]
 
-    #Generacja instancji
-    x = RandomNumberGenerator(20)
-    A = 0
-    for i in range(3):
-        for j in range(n):
-            p = x.nextInt(1, 99)
-            tablep[i][j] = p
-            A = A + p
-    B = math.floor(0.5*A)
-    A = math.floor((1/6)*A)
+            #Generacja instancji
+            x = RandomNumberGenerator(20)
+            A = 0
+            for i in range(3):
+                for j in range(n):
+                    p = x.nextInt(1, 99)
+                    tablep[i][j] = p
+                    A = A + p
+            B = math.floor(0.5*A)
+            A = math.floor((1/6)*A)
 
-    for j in range(n):
-        d = x.nextInt(A, B)
-        tabled[j] = d
+            for j in range(n):
+                d = x.nextInt(A, B)
+                tabled[j] = d
 
+            #Główna pętla programu
 
+            P = []
+            Pscores = []
+            it = 0
 
+            x = randomSolution()
+            makespan, maxTardiness = checkSolution(x, tablep, tabled)
+            P.append(x)
+            Pscores.append([makespan, maxTardiness])
 
+            while it < maxIter:
+                
+                xPrim = movefunction("insert", x)
+                makespanPrim, maxTardinessPrim = checkSolution(xPrim, tablep, tabled)
+
+                boolResult = domination(makespan, maxTardiness,
+                                        makespanPrim, maxTardinessPrim)
+
+                if boolResult:
+                    x = xPrim
+                    makespan = makespanPrim
+                    maxTardiness = maxTardinessPrim
+                    P.append(x)
+                    Pscores.append([makespan, maxTardiness])
+                else:
+                    if random.random() < acceptProbability(1, it):
+                        x = xPrim
+                        makespan = makespanPrim
+                        maxTardiness = maxTardinessPrim
+                        P.append(x)
+                        Pscores.append([makespan, maxTardiness])
+                it = it+1
+
+            #Pareto
+            F, Fscore = frontParetoF(P, tablep, tabled)
+
+            #Wykresy i HVI
+            print(str(iteration)+", "+str(maxIter))
+
+        
 
 main()
